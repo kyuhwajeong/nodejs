6e6e033a4fcc6fef024aa3c9acc3767b8c382040
정규화 <kuha0879@gmail.com>
2018-06-12 23:38:43 +0900

    nodejs 설치 및 웹서버 설정, 파일로 web1 동적 페이지 처리

---
+++ syntax/template.js
@@ -0,0 +1,17 @@
+var name = 'k8805';
+// String literals
+var letter = 'Dear '+name+' \n\
+\n\
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '+name+' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. '+name;
+
+
+var letter = `Dear ${name}
+ 
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ${name} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ${1+1} Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. ${name}`;
+
+
+console.log(letter);
+
+
+//var a = 1; //숫자라는 리터럴
+//var a = '1'; //정보를 표현하는 기호, 방법
