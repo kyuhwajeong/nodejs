156d81e42cb6238c94daaac2316e24c7b8105df7
정규화 <kuha0879@gmail.com>
2018-06-13 00:55:34 +0900

    배열, 반복문, 반복문을 이용한 배열값 합계 확인

---
+++ syntax/array-loop.js
@@ -0,0 +1,10 @@
+var number = [1,400, 12, 34];
+var i = 0, total = 0;
+
+while (i < number.length) {
+  total+=number[1];
+  i+=1;
+}
+console.log(`total : ${total}`);
+
+// 반복문을 이용한 배열값 합계
---
+++ syntax/array.js
@@ -0,0 +1,14 @@
+//CRUD
+
+// array literal
+var arr = ['A','B','C','D'];
+console.log(arr[1]);
+console.log(arr[3]);
+
+arr[2] = 3; //변경
+console.log(arr);
+
+console.log(arr.length); //길이
+
+arr.push('E'); // 추가
+console.log(arr);
