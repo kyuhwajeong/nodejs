4dd49d8a32798120b9cd8daf1c058d2afe23a643
정규화 <kuha0879@gmail.com>
2018-06-13 18:43:43 +0900

    객체를 이용해서 템블릿 기능 정리정돈(개별함수를 객체로 접근하도록 변경)

--- main.js
+++ main.js
@@ -2,34 +2,33 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-function templateHTML(title, list, body, control){
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB2 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-  </body>
-  </html>
-  `;
-}
-
-function templateList(filelist){
-  var list = '<ul>';
-  var i = 0;
-  while (i < filelist.length) {
-    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i+=1;
+var template = {
+  HTML:function(title, list, body, control){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB2 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+    </html>
+    `;
+  },List:function(filelist){
+    var list = '<ul>';
+    var i = 0;
+    while (i < filelist.length) {
+      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i+=1;
+    }
+    list+='</ul>';
+    return list;
   }
-  list+='</ul>';
-  return list;
 }
 
 var app = http.createServer(function(request,response){
@@ -43,18 +42,18 @@ var app = http.createServer(function(request,response){
         fs.readdir(`./data`, function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = templateList(filelist);
-          var template = templateHTML(title, list,`<h2>${title}</h2>${description}`,
+          var list = template.List(filelist);
+          var html = template.HTML(title, list,`<h2>${title}</h2>${description}`,
           `<a href="/create">create</a>`);
             response.writeHead(200); // 웹서버 연결이 정상
-            response.end(template);
+            response.end(html);
         })
       } else {
         fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var list = templateList(filelist);
-            var template = templateHTML(title, list,`<h2>${title}</h2>${description}`,
+            var list = template.List(filelist);
+            var html = template.HTML(title, list,`<h2>${title}</h2>${description}`,
             `<a href="/create">create</a>
              <a href="/update?id=${title}">update</a>
              <form action="delete_process" method="post">
@@ -63,15 +62,15 @@ var app = http.createServer(function(request,response){
              </form>
             `);
             response.writeHead(200); // 웹서버 연결이 정상
-            response.end(template);
+            response.end(html);
         });
       });
       }
   } else if(pathname === '/create'){  // 생성 폼
     fs.readdir(`./data`, function(error, filelist){
       var title = 'WEB - create';
-      var list = templateList(filelist);
-      var template = templateHTML(title, list,`<form action="/create_process"
+      var list = template.List(filelist);
+      var html = template.HTML(title, list,`<form action="/create_process"
       method="post">
       <p><input type="text" name ="title" placeholder="title"></p>
       <p>
@@ -81,7 +80,7 @@ var app = http.createServer(function(request,response){
       </form>
       `,``);
         response.writeHead(200); // 웹서버 연결이 정상
-        response.end(template);
+        response.end(html);
     })
   } else if(pathname === '/create_process'){
     var body = '';
@@ -102,8 +101,8 @@ var app = http.createServer(function(request,response){
     fs.readdir(`./data`, function(error, filelist){
       fs.readFile(`data/${queryData.id}`,'utf8', function(err, description){
       var title = queryData.id;
-      var list = templateList(filelist);
-      var template = templateHTML(title, list,
+      var list = template.List(filelist);
+      var html = template.HTML(title, list,
         `<form action="/update_process" method="post">
         <p><input type="hidden" name ="id" value="${title}"></p>
         <p><input type="text" name ="title" placeholder="title" value="${title}"></p>
@@ -114,7 +113,7 @@ var app = http.createServer(function(request,response){
         </form>
         `,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
         response.writeHead(200); // 웹서버 연결이 정상
-        response.end(template);
+        response.end(html);
       });
     });
   } else if(pathname === '/update_process'){
