24fcf79896664f21a56e7e20c865dba1ebbdf267
정규화 <kuha0879@gmail.com>
2018-06-13 16:27:09 +0900

    서브 메뉴 선택에 대한 글 수정 링크 생성

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -44,7 +44,8 @@ var app = http.createServer(function(request,response){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = templateList(filelist);
-          var template = templateHTML(title, list,`<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,`<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`);
             response.writeHead(200); // 웹서버 연결이 정상
             response.end(template);
         })
@@ -53,7 +54,8 @@ var app = http.createServer(function(request,response){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = templateList(filelist);
-            var template = templateHTML(title, list,`<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list,`<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a> <a href="/update=${title}">update</a>`);
             response.writeHead(200); // 웹서버 연결이 정상
             response.end(template);
         });
@@ -71,7 +73,7 @@ var app = http.createServer(function(request,response){
       </p>
       <p><input type="submit"></p>
       </form>
-      `);
+      `,``);
         response.writeHead(200); // 웹서버 연결이 정상
         response.end(template);
     })
