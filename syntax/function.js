b571b4e23c65731863d8211fb7c8dfde6070f34b
정규화 <kuha0879@gmail.com>
2018-06-13 13:40:20 +0900

    function 사용방법

---
+++ syntax/function.js
@@ -0,0 +1,20 @@
+f123()
+console.log('A');
+console.log('Z');
+console.log('B');
+f123()
+console.log('F');
+console.log('C');
+console.log('P');
+console.log('J');
+f123()
+console.log('A');
+console.log('Z');
+console.log('B');
+
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
---
+++ syntax/function2.js
@@ -0,0 +1,15 @@
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+
+function sum(first, second) // parameter
+{
+  //console.log(first + second);
+  console.log('a');
+  return first+second;
+  console.log('b');
+}
+
+
+//sum(2,4); // argument (value)
+console.log(sum(2,4));
