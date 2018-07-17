f1b001551632fb6914476e5cec96274c7ad03f50
정규화 <kuha0879@gmail.com>
2018-07-18 02:04:43 +0900

    Node.js 코드의 정리정돈 (topic)

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var qs = require('querystring');
 
 var template = require('./lib/template.js');
 var db = require('./lib/db');
+var topic = require('./lib/topic')
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -13,150 +14,20 @@ var app = http.createServer(function(request,response){
     if(pathname === '/'){
       if(queryData.id === undefined)
       {
-        db.query(`SELECT * FROM topic`,function (error, topics) {
-            console.log(topics);
-            var title = 'Welcome';
-            var description = 'Hello, Node.js';
-            var list = template.List(topics);
-            var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`);
-            response.writeHead(200);
-            response.end(html);
-        });
+        topic.home(request, response);
       } else {
-      db.query(`SELECT * FROM topic`,function (error, topics) {
-        if(error){
-          throw error;
-        }
-        db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,[queryData.id], function(error2, topic){
-          if(error2){
-            throw error2;
-          }
-          var title = topic[0].title;
-          var description = topic[0].description;
-          var list = template.List(topics);
-          var html = template.HTML(title, list,
-          `<h2>${title}</h2>${description}
-          <p>by ${topic[0].name}</p>`,
-          `<a href="/create">create</a>
-           <a href="/update?id=${queryData.id}">update</a>
-           <form action="delete_process" method="post">
-            <input type = "hidden" name = "id" value = "${queryData.id}">
-            <input type = "submit" value = "delete">
-           </form>
-          `);
-          response.writeHead(200);
-          response.end(html);
-        })
-      });
+        topic.page(request, response);
       }
   } else if(pathname === '/create'){  // 생성 폼
-    db.query(`SELECT * FROM topic`,function (error, topics) {
-      db.query(`SELECT * FROM author`,function(error2, authors){
-        //console.log(topics);
-        var title = 'Create';
-        var list = template.List(topics);
-        var html = template.HTML(title, list,
-        `
-        <form action="/create_process" method="post">
-          <p><input type="text" name ="title" placeholder="title"></p>
-          <p>
-            <textarea name = "description" placeholder="description"></textarea>
-          </p>
-          <p>
-            ${template.authorSelect(authors)}
-          </p>
-          <p><input type="submit"></p>
-        </form>
-        `,
-        `<a href="/create">create</a>`);
-        response.writeHead(200);
-        response.end(html);
-     });
-   });
+    topic.create(request, response);
   } else if(pathname === '/create_process'){
-    var body = '';
-    request.on('data', function(data){ // post로 데이터가 조각조각 들어옴
-      body+=data;
-    });
-    request.on('end', function(){ //
-      var post = qs.parse(body);
-      //console.log(post);
-      db.query(`
-        INSERT INTO topic (title, description, created, author_id)
-        VALUES(?,?, NOW(), ?)`,
-      [post.title, post.description, post.author],
-      function(error, result){
-        if(error){
-          throw error;
-        }
-        response.writeHead(302, {Location: `/?id=${result.insertId}`});
-        response.end();
-      }
-     )
-    });
+    topic.create_process(request, response);
   } else if(pathname === '/update'){  // 수정 폼
-    db.query('SELECT * FROM topic', function(error, topics){
-        if(error){
-          throw error;
-        }
-        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
-          if(error2){
-            throw error2;
-          }
-          db.query('SELECT * FROM author', function(error2, authors){
-          var list = template.List(topics);
-          var html = template.HTML(topic[0].title, list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-              </p>
-              <p>
-                ${template.authorSelect(authors, topic[0].author_id)}
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-         });
-        });
-      });
+    topic.update(request, response);
   } else if(pathname === '/update_process'){
-    var body = '';
-     request.on('data', function(data){
-         body += data;
-     });
-     request.on('end', function(){
-         var post = qs.parse(body);
-         db.query('UPDATE topic SET title=?, description=?, author_id=? WHERE id=?', [post.title, post.description, post.author, post.id], function(error, result){
-           response.writeHead(302, {Location: `/?id=${post.id}`});
-           response.end();
-         })
-     });
+    topic.update_process(request, response);
   } else if(pathname === '/delete_process'){ // 삭제 처리
-    var body = '';
-          request.on('data', function(data){
-              body = body + data;
-          });
-          request.on('end', function(){
-              var post = qs.parse(body);
-              db.query('DELETE FROM topic WHERE id = ?', [post.id], function(error, result){
-                if(error){
-                  throw error;
-                }
-                response.writeHead(302, {Location: `/`});
-                response.end();
-              });
-          });
+    topic.delete_process(request, response);
   } else {
     response.writeHead(404);
     response.end('Not found');
