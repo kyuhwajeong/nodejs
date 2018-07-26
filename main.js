4bb1f765b2acc0b640d944a03b02dcd1432a0271
정규화 <kuha0879@gmail.com>
2018-07-26 23:26:26 +0900

    상세보기 페이지 구현 query string을 사용하지 않는 pretty url(clean url, semantic url..)로 라우트 기능을 구현하는 방법

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 var fs = require('fs');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 var template = require('./lib/template.js');
 
 //route, routing
@@ -18,8 +20,28 @@ app.get('/', function(request, response) {
     });
 });
 
-app.get('/page', function(req, res) {
-  return res.send('/page');
+app.get('/page/:pageId', function(request, response) {
+  fs.readdir('./data', function(error, filelist){
+      var filteredId = path.parse(request.params.pageId).base;
+      fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+        var title = request.params.pageId;
+        var sanitizedTitle = sanitizeHtml(title);
+        var sanitizedDescription = sanitizeHtml(description, {
+          allowedTags:['h1']
+        });
+        var list = template.List(filelist);
+        var html = template.HTML(sanitizedTitle, list,
+          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+          ` <a href="/create">create</a>
+            <a href="/update?id=${sanitizedTitle}">update</a>
+            <form action="delete_process" method="post">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
+              <input type="submit" value="delete">
+            </form>`
+        );
+        response.send(html);
+      });
+    });
 });
 
 app.listen(3000, function() {
