e8a7a9e2bd41aa53ee1daeeb7fb9dd1fa3fb7327
정규화 <kuha0879@gmail.com>
2018-07-27 00:25:47 +0900

    Express 미들웨어의 사용 body-parser를 이용해서 post 방식으로 전송된 form 데이터를 쉽게 가져오는 방법

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ const app = express()
 var fs = require('fs');
 var path = require('path');
 var qs = require('querystring');
+var bodyParser = require('body-parser');
 var sanitizeHtml = require('sanitize-html');
 var template = require('./lib/template.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //route, routing
 //app.get('/', (req, res) => res.send('Hello World!'))
@@ -65,19 +68,13 @@ app.get('/create', function(request, response){
 });
 
 app.post('/create_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
-      })
-  });
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+    response.writeHead(302, {Location: `/?id=${title}`});
+    response.end();
+  })
 });
 
 app.get('/update/:pageId', function(request, response){
@@ -107,37 +104,25 @@ app.get('/update/:pageId', function(request, response){
 });
 
 app.post('/update_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(error){
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end();
-        })
-      });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.writeHead(302, {Location: `/?id=${title}`});
+      response.end();
+    })
   });
 });
 
 app.post('/delete_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function(error){
-        response.redirect('/');
-      })
-  });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error){
+    response.redirect('/');
+  })
 });
 
 app.listen(3000, function() {
